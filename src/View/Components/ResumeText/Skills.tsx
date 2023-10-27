6598c629ea9faa27ab81fa3660185e67ad8fc7a5
WilliamHarer <46909464+WilliamHarer@users.noreply.github.com>
2023-10-27 01:44:14 -0700

    Add text to skills section

---
+++ src/View/Components/ResumeText/Skills.tsx
@@ -0,0 +1,19 @@
+export default function skills() {
+    return(
+        <div>
+        <p className="c4"><span className="c6 c14">Skills</span>
+            <hr/>
+        </p>
+        <p className="c4"><span className="c6">Analysis:</span><span className="c0">&nbsp;Topological Data Analysis (introductory)</span>
+        </p>
+        <p className="c4"><span className="c6">IDEs:</span><span
+            className="c0">&nbsp;intelliJ, Eclipse, Vim, Sublime, Jupyter Notebook</span></p>
+        <p className="c4"><span className="c6">Programming Languages: </span><span className="c0">Python, C, Java, javaScript, Kotlin, Typescript</span>
+        </p>
+        <p className="c4"><span className="c6">Technologies: </span><span className="c0">OpenCV, MERN, Flask, React, React-Redux, React-Saga, AWS, Linux/Unix, Systems programming, NPM, NVM, PACT, Jenkins, Sinon, Jest, Mocha/Chai, Vite, Pnpm, Cypress, Jira, docker </span>
+        </p>
+        <p className="c4"><span className="c6">Personal Interests: </span><span className="c0">Music: Currently playing Electric Bass, Gaming, Competitive Programming</span>
+        </p>
+        </div>
+    )
+}
